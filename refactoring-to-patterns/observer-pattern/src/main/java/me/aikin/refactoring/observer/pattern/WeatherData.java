1ce137dafbec36c9848380e8e889a8200297d0c5
tabismacbook <tabindaqavi1@gmail.com>
2022-10-19 12:42:41 +0500

    refactored observer implementation

--- refactoring-to-patterns/observer-pattern/src/main/java/me/aikin/refactoring/observer/pattern/WeatherData.java
+++ refactoring-to-patterns/observer-pattern/src/main/java/me/aikin/refactoring/observer/pattern/WeatherData.java
@@ -3,14 +3,8 @@ package me.aikin.refactoring.observer.pattern;
 import java.util.Observable;
 
 public class WeatherData extends Observable {
-    private final SeedingMachine seedingMachine;
-    private final ReapingMachine reapingMachine;
-    private final WateringMachine wateringMachine;
 
-    public WeatherData(SeedingMachine seedingMachine, ReapingMachine reapingMachine, WateringMachine wateringMachine) {
-        this.seedingMachine = seedingMachine;
-        this.reapingMachine = reapingMachine;
-        this.wateringMachine = wateringMachine;
+    public WeatherData() {
     }
 
     public void measurementsChanged(WeatherParams weatherParams) {
--- refactoring-to-patterns/observer-pattern/src/test/java/me/aikin/refactoring/observer/pattern/WeatherDataTest.java
+++ refactoring-to-patterns/observer-pattern/src/test/java/me/aikin/refactoring/observer/pattern/WeatherDataTest.java
@@ -15,12 +15,14 @@ public class WeatherDataTest {
     private final WeatherData weatherData;
 
     public WeatherDataTest() {
-        weatherData = new WeatherData(seedingMachine, reapingMachine, wateringMachine);
+        weatherData = new WeatherData();
+        weatherData.addObserver(seedingMachine);
+        weatherData.addObserver(reapingMachine);
+        weatherData.addObserver(wateringMachine);
     }
 
     @Test
     public void seeding_machine_should_start_if_temperature_over_5_degree() {
-        weatherData.addObserver(seedingMachine);
         weatherData.measurementsChanged(new WeatherParams(10, 0, 0));
 
         assertTrue(seedingMachine.getStatus());
@@ -28,21 +30,18 @@ public class WeatherDataTest {
 
     @Test
     public void reaping_machine_should_start_if_temperature_over_5_degree_and_humidity_over_65() {
-        weatherData.addObserver(reapingMachine);
         weatherData.measurementsChanged(new WeatherParams(10, 70, 0));
         assertTrue(reapingMachine.getStatus());
     }
 
     @Test
     public void water_machine_should_start_if_temperature_over_10_degree_and_humidity_less_than_55_and_wind_power_less_than_4() {
-        weatherData.addObserver(wateringMachine);
         weatherData.measurementsChanged(new WeatherParams(12, 50, 2));
         assertTrue(wateringMachine.getStatus());
     }
 
     @Test
     public void reaping_machine_should_not_start_if_temperature_under_5_degree_and_humidity_over_65() {
-        weatherData.addObserver(reapingMachine);
         weatherData.measurementsChanged(new WeatherParams(3, 70, 0));
         assertNull(reapingMachine.getStatus());
     }
